e3204ff426c4febdd94e445972c21e3d4b09c7ce
HermelaDemeke <bhmd0595@gmail.com>
2023-10-29 01:02:19 +0300

    completed OOP homework

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,21 @@
 import java.util.ArrayList;
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         //create Student objects
-        Student student1=new Student("demeke",1,new int[]{85,90,95});
-        Student student2=new Student("beletu",2,new int[]{87,89,99});
+        Student student1 = new Student("demeke", 1, new int[]{85, 90, 95});
+        Student student2 = new Student("beletu", 2, new int[]{87, 89, 99});
         //create Teacher objects
-        Teacher teacher1=new Teacher("abeje","physics");
-        Teacher teacher2=new Teacher("tigist","biology");
+        Teacher teacher1 = new Teacher("abeje", "physics");
+        Teacher teacher2 = new Teacher("tigist", "biology");
         //create College objects
-        College college1=new College("Informatics",new ArrayList<Student>(),new ArrayList<Teacher>());
+        College college1 = new College("Informatics", new ArrayList<Student>(), new ArrayList<Teacher>());
         // Add Student and Teacher objects to the college
         college1.getStudents().add(student1);
         college1.getStudents().add(student2);
         college1.getTeachers().add(teacher1);
         college1.getTeachers().add(teacher2);
         //print out details
-       System.out.println("College Name: " + College.getName());
+        System.out.println("College Name: " + College.getName());
         System.out.println("Students:");
         for (Student s : College.getStudents()) {
             System.out.println("Name: " + s.getName());
@@ -29,6 +29,6 @@ public class Main {
             System.out.println("Subject: " + t.getSubject());
             System.out.println();
         }
-
+    }
 }
 
--- src/Student.java
+++ src/Student.java
@@ -3,17 +3,19 @@ public class Student {
     private String name;
     private int id;
     private int[] grades;
+
     //constructor
-    public Student(String name,int id,int[] grades){
-        this.name=name;
-        this.id=id;
-        this.grades=grades;
+    public Student(String name, int id, int[] grades) {
+        this.name = name;
+        this.id = id;
+        this.grades = grades;
     }
     //getter and setter for students attribute
 
     public String getName() {
         return name;
     }
+
     public void setName(String name) {
         this.name = name;
     }
@@ -21,15 +23,18 @@ public class Student {
     public int getId() {
         return id;
     }
+
     public void setId(int id) {
         this.id = id;
     }
+
     public int[] getGrades() {
         return grades;
     }
-    public void setGrades(int[] grades){
-        this.grades=grades;
-    }
 
+    public void setGrades(int[] grades) {
+        this.grades = grades;
+    }
+}
 
 
